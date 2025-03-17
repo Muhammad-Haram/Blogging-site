6c9b0e5fe7696dc2143938f37d07f6846c920730
Muhammad Haram <haramh643@gmail.com>
2025-03-17 14:10:26 +0500

    add blo veiw page and comments

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const multer = require('multer')
 const path = require('path')
-const Blog = require('../models/blog')
+
+const Blog = require('../models/blog');
+const Comment = require("../models/comment")
 
 const router = Router();
 
@@ -35,6 +37,25 @@ router.post('/', upload.single('coverImage'), async (req, res) => {
     })
 
     return res.redirect(`/blog/${blog._id}`);
+})
+
+router.get('/:id', async (req, res) => {
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy")
+    return res.render("blog", {
+        user: req.user,
+        blog,
+        comments
+    })
+})
+
+router.post('/comment/:blogId', async (req, res) => {
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id
+    })
+    return res.redirect(`/blog/${req.params.blogId}`)
 })
 
 module.exports = router;
