1f0de14ff5058aeffbf10b598e7cffd7e967897c
Muhammad Haram <haramh643@gmail.com>
2025-03-17 11:15:19 +0500

    add blogs and multer.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser')
+const Blog = require('./models/blog')
 
 const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
 const connectToDb = require("./connect");
 const dotenv = require("dotenv");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
@@ -20,14 +23,21 @@ app.set("views", path.resolve("./views"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(checkForAuthenticationCookie("token"))
+app.use(express.static(path.resolve('./public')))
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+
+  const allBlogs = await Blog.find({})
+
   res.render("home", {
-    user: req.user
+    user: req.user,
+    blogs: allBlogs
   });
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
